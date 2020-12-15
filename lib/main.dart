32a2e216f7caa27ebe5c0c80780d0037e2bac11e
Abed <49130097+abddrk23@users.noreply.github.com>
2020-12-15 12:42:28 +0200

    Flutter

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'home/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: _SplashScreen(),
+    );
+  }
+}
+
+class _SplashScreen extends StatefulWidget {
+  @override
+  __SplashScreenState createState() => __SplashScreenState();
+}
+
+class __SplashScreenState extends State<_SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Timer(
+    //     Duration(seconds: 3),
+    //     () => Navigator.push(
+    //         context, MaterialPageRoute(builder: (context) => MyHome())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image(
+            image: AssetImage('images/facebook_logo.png'),
+            width: width * 0.5,
+            height: height * 0.5,
+          ),
+          SpinKitDoubleBounce(
+            color: Colors.blueGrey[600],
+          )
+        ],
+      ),
+    );
+  }
+}
